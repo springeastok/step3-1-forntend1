3bac49b8a7d66a728ed07cef4e8246d780040822
springeastok <springeastok@gmail.com>
2025-08-31 06:55:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
+  output: 'standalone',
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  }
 }
 
 module.exports = nextConfig
